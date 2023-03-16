1eb96e7b7c756e0578d7969087c3da9361cd1063
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-16 14:49:12 +0900

    Problem5 solved

--- src/main/java/onboard/Problem5.java
+++ src/main/java/onboard/Problem5.java
@@ -3,6 +3,20 @@ package onboard;
 public class Problem5 {
     public static Integer solution(Integer N, Integer K, Integer[] values){
         Integer answer = 0;
+        int coin=N-1;
+        while(true) {
+        	if(K<values[coin]) coin--;
+        	else if(K==values[coin]) break;
+        	else break;
+        }
+        while(K>0) {
+        	if(K>=values[coin]) {
+        		answer+=K/values[coin];
+        		K=K-values[coin]*(K/values[coin]);
+        	}
+        	coin--;
+        	if(coin<0) break;
+        }
         return answer;
     }
 }
