e98b9c311a62bb5f0367376bd10f144c303bb6f0
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-16 14:47:15 +0900

    Problem3 solved

--- src/main/java/onboard/Problem3.java
+++ src/main/java/onboard/Problem3.java
@@ -6,6 +6,13 @@ import java.util.List;
 public class Problem3 {
     public static List<Integer> solution(List<Integer> numbers){
         List<Integer> answer = new ArrayList<>();
+        Collections.sort(numbers);
+        int avg=numbers.get(2);
+        int sum=0;
+        for(int i=0;i<5;i++) {
+        	sum+=numbers.get(i);
+        }
+        answer.add(avg); answer.add(sum/5);
         return answer;
     }
 }
