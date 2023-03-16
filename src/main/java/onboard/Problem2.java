f241031b4aa34cc345edbbe41f8602e63986f907
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-16 14:45:28 +0900

    Problem2 solved

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -5,7 +5,18 @@ import java.util.List;
 
 public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
-        Integer[] answer = {};
+        Integer[] answer = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        for(int i=0;i<10;i++) {
+        	int start=flipCards[i][0];
+        	int end=flipCards[i][1];
+        	while(start<end) {
+        		int num=answer[start];
+        		answer[start]=answer[end];
+        		answer[end]=num;
+        		start++;
+        		end--;
+        	}
+        }
         return answer;
     }
 }
