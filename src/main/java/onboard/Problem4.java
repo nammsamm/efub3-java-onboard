45926b15626a8e3e4a16b094c198fde918de4752
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-16 14:48:14 +0900

    Problem4 solved

--- src/main/java/onboard/Problem4.java
+++ src/main/java/onboard/Problem4.java
@@ -7,6 +7,20 @@ package onboard;
 public class Problem4 {
     public static Integer solution(Integer number){
         Integer answer = 0;
+        int ten=1;
+        int j=0;
+        List<Integer> numbers=new ArrayList<>();
+        while(number!=0) {
+        	numbers.add(number%10);
+        	number/=10;
+        	ten*=10;
+        	j++;
+        }
+        Collections.sort(numbers, Collections.reverseOrder());
+        for(int i=0;i<j;i++) {
+        	ten/=10;
+        	answer+=(numbers.get(i))*ten;
+        }
         return answer;
     }
 }
