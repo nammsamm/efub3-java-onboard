954a6dd6b85169ec2edb8f85f32d14c176144054
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-17 14:00:41 +0900

    Update Problem2.java

--- src/main/java/onboard/Problem2.java
+++ src/main/java/onboard/Problem2.java
@@ -7,6 +7,10 @@ public class Problem2 {
     public static Integer[] solution(Integer[][] flipCards){
         Integer[] answer = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
         for(int i=0;i<10;i++) {
+        	flipCards[i][0]=flipCards[i][0]-1;
+        	flipCards[i][1]=flipCards[i][1]-1;
+        }
+        for(int i=0;i<10;i++) {
         	int start=flipCards[i][0];
         	int end=flipCards[i][1];
         	while(start<end) {
