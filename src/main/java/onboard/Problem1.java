dbd41e6d1d93f1db2590d555cca6ad2a707dcce7
nammsamm <108855492+nammsamm@users.noreply.github.com>
2023-03-17 13:45:41 +0900

    Update Problem1.java

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -7,8 +7,8 @@ public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
         List<Integer> answer = new ArrayList<>();
         int time=hour*60+minute-45;
-		if(time<0) hour=23; minute=time+60;
-		else hour=time/60; minute=time%60;
+		if(time<0) {hour=23; minute=time+60;}
+		else {hour=time/60; minute=time%60;}
         answer.add(hour); answer.add(minute);
         return answer;
     }
